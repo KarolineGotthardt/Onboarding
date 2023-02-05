21ea17358b8151f447a0ba23093b285314ab986e
karolinegotthardt <karoline.gotthardt@horizon-alpha.com>
2023-02-05 14:52:29 +0100

    Tutorial until 2:42:00 https://www.youtube.com/watch?v=ABQLwlE8MUA&list=PL0soZbXxEUOS-nfqTnawBkqxinYY3axMa&index=2

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
 
+const products = [
+    "tooth paste",
+    "tooth brush",
+    "mouth wash",
+    "dental floss",
+    "mouth guard"
+];
+
 const SearchBar = () => {
     const [searchValue, setSearchValue] = useState("")
+    const shouldDisplayClearBtn = searchValue.length > 0
+    const filteredProducts = products.filter(
+        (product) => {return product.includes(searchValue)}
+    )
 
     const handleInputChange = (event) => {
         setSearchValue(event.target.value)
@@ -15,9 +27,20 @@ const SearchBar = () => {
     return (
         <div>
             <input type="text" value={searchValue} onChange={handleInputChange}/>
-            <button onClick={handleClearClick}>clear</button>
+            {shouldDisplayClearBtn &&
+                <button onClick={handleClearClick}>clear</button>}
+
+            <ul>
+                {
+                    filteredProducts.map(
+                        (product) => {
+                    return <li key={product}>{product}</li>}
+                    )
+                }
+            </ul>
+
         </div>
     )
-}
+};
 
-export default SearchBar
+export default SearchBar;
