4a72d01fca8ae02d760dc63de64961a8a4a6797e
karolinegotthardt <karoline.gotthardt@horizon-alpha.com>
2023-02-06 14:56:05 +0100

    Academind Tutorial until 0:55:00 https://www.youtube.com/watch?v=Dorf8i6lCuk&list=PL0soZbXxEUOS-nfqTnawBkqxinYY3axMa&index=3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Todo from "./components/Todo";
+
 /*
 React Component
 returns React Element in JSX (Combination of JavaScript & HTML Syntax) to render what you see in the browser
@@ -6,12 +8,13 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo />
+        <br/>
+      <Todo />
+        <br/>
+      <Todo />
+        <br/>
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+function Todo(){
+    return (
+        <div className="card">
+            <h2>TITLE</h2>
+            <div className="actions">
+                <button className="btn">Delete</button>
+            </div>
+        </div>
+    )
+}
+
+/*
+Make exportable: https://stackoverflow.com/questions/21117160/what-is-export-default-in-javascript
+ */
+export default Todo;
