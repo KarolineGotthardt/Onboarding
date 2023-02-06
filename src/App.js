988055d64a43fb896bd069ffcbd7544972ab6029
karolinegotthardt <karoline.gotthardt@horizon-alpha.com>
2023-02-06 14:42:58 +0100

    Academind Tutorial until 0:48:00 https://www.youtube.com/watch?v=Dorf8i6lCuk&list=PL0soZbXxEUOS-nfqTnawBkqxinYY3axMa&index=3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-
+/*
+React Component
+returns React Element in JSX (Combination of JavaScript & HTML Syntax) to render what you see in the browser
+ */
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <div className="card">
+        <h2>TITLE</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+/*
+entry point to script in browser
+
+https://reactjs.org/docs/glossary.html#react-elements
+https://stackoverflow.com/questions/30971395/difference-between-react-component-and-react-element#:~:text=React%20Element%20%2D%20It%20is%20a,apply%20any%20methods%20on%20it.&text=React%20Component%20%2D%20It%20is%20a,and%20returns%20a%20React%20element.
+*/
+ReactDOM.render(
+    <App/>, document.getElementById("root")
+);
