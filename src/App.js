94b0e7ee0caad619851cac8b5a643dc47627186c
karolinegotthardt <karoline.gotthardt@horizon-alpha.com>
2023-02-07 13:31:16 +0100

    Academind Tutorial until 1:02:00 https://www.youtube.com/watch?v=Dorf8i6lCuk&list=PL0soZbXxEUOS-nfqTnawBkqxinYY3axMa&index=3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetups from "./pages/NewMeetups";
 import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   /*
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <div>
+        <MainNavigation/>
       <Routes>
         <Route path="/" element={<AllMeetups />} />
         <Route path="/new-meetup" element={<NewMeetups />} />
